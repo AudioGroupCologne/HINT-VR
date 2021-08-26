5ecbe7dbeb8788df725df658ba716161b0ae988b
Alexander Müller <am@imagineon.de>
2021-08-26 14:27:24 +0200

    Added gravity and adapted player size to level
    Reference: https://www.youtube.com/watch?v=_QajrabyTJc

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,17 +7,35 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
 
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0f)
+        {
+            // don't use 0f to maintain a constant pull to the ground
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        // TODO: maybe rewrite mouselook to move player not just camera
         // use this instead of Vector(x,y,z) to control movement depending on the direction the player is looking
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
 
     }
 }
