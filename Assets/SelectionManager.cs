a4ab912c59a68855e94c7c87ddce2e353d5e08a3
Alexander Müller <am@imagineon.de>
2021-08-27 12:08:44 +0200

    Added 'selection' option (rough)
    Source: https://www.youtube.com/watch?v=_yf5vzZ2sYE

---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material highlightMaterial;
+
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        // center of screen will always equal mouse position
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    selectionRenderer.material = highlightMaterial;
+                }
+                _selection = selection;
+            }
+        }
+    }
+}
