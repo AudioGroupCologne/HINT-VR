6960bba9ab349d6316b9e94a0a9cc098a9740422
Alexander <55482813+alexm-imag@users.noreply.github.com>
2021-10-29 13:45:15 +0200

    Added master volume to settings

--- Assets/MyAssets/Scripts/Menus/SettingsManager.cs
+++ Assets/MyAssets/Scripts/Menus/SettingsManager.cs
@@ -2,49 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Toggle toggleStatic;
-    public Toggle toggleMoving;
-    public Toggle toggleVarRadius;
+    [SerializeField] Slider volume;
+    [SerializeField] AudioMixer mixer;
 
-    bool MovingCubeState;
-    bool StaticCubeState;
-    bool VarRadState;
-
-    void Start()
+    private void Start()
     {
-        MovingCubeState = (PlayerPrefs.GetInt("MovingOn") == 1) ? true : false;
-        StaticCubeState = (PlayerPrefs.GetInt("StaticOn") == 1) ? true : false;
-        VarRadState = (PlayerPrefs.GetInt("VarRadOn") == 1) ? true : false;
-
-        toggleStatic.isOn = StaticCubeState;
-        toggleMoving.isOn = MovingCubeState;
-
-        if (MovingCubeState)
-        {
-            toggleVarRadius.isOn = VarRadState;
-        }
+        volume.Select();
     }
 
-    public void ToggleStaticChanged()
+    public void SilderChanged()
     {
-        // Get isOn from Toggle and store it in PlayerPrefs to access it in other scenes
-        PlayerPrefs.SetInt("StaticOn", toggleStatic.isOn ? 1 : 0);
+        Debug.Log("Volume Slider: " + volume.value);
+        mixer.SetFloat("MasterVol", volume.value);
     }
 
-    public void ToggleMovingChanged()
-    {
-        MovingCubeState = toggleMoving.isOn;
-        PlayerPrefs.SetInt("MovingOn", toggleMoving.isOn ? 1 : 0);
-        // VarRadius shall only be active, when MovingCube is enabled!
-    }
-
-    public void ToggleVarRadChanged()
-    {
-        PlayerPrefs.SetInt("VarRadOn", toggleVarRadius.isOn ? 1 : 0);
-    }
 
 }
