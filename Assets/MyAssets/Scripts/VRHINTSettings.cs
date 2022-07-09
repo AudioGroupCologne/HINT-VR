c31f3d2e654e120dd952e1869115b78f352dd4fd
Alexander Müller <am@imagineon.de>
2022-07-09 15:02:04 +0200

    Added UI elements for 1st/2nd test condition

--- Assets/MyAssets/Scripts/VRHINTSettings.cs
+++ Assets/MyAssets/Scripts/VRHINTSettings.cs
@@ -8,9 +8,13 @@ public class VRHINTSettings : MonoBehaviour
 {
 
     [SerializeField] GameObject settings;
+    [SerializeField] GameObject settingsGroup1;
+    [SerializeField] GameObject settingsGroup2;
 
     public delegate void OnSettingsDone(feedbackSettings setting);
     public OnSettingsDone OnSettingsDoneCallback = delegate { Debug.Log("No settingsDone delegate set!"); };
+
+    private int settingState = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +28,7 @@ public class VRHINTSettings : MonoBehaviour
         if (!settings.activeSelf)
             return;
 
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             ButtonHandler(1);
         }
@@ -36,24 +40,59 @@ public class VRHINTSettings : MonoBehaviour
         {
             ButtonHandler(3);
         }
+        
     }
 
 
     public void ButtonHandler(int setting)
     {
-        switch(setting)
+
+        if (settingState == 0)
         {
-            case 1:
-                OnSettingsDoneCallback(feedbackSettings.classic);
-                break;
-            case 2:
-                OnSettingsDoneCallback(feedbackSettings.wordSelection);
-                break;
-            case 3:
-                OnSettingsDoneCallback(feedbackSettings.comprehensionLevel);
-                break;
+            switch (setting)
+            {
+                case 1:
+                    Debug.Log("First test");
+                    break;
+                case 2:
+                    Debug.Log("Second test");
+                    break;
+                default:
+                    Debug.Log("Invalid selection!");
+                    return;
+            }
+
+            settingState = 1;
+            settingsGroup1.SetActive(false);
+            settingsGroup2.SetActive(true);
+
         }
-        ShowSettings(false);
+
+        else if(settingState == 1)
+        {
+            switch (setting)
+            {
+                case 1:
+                    OnSettingsDoneCallback(feedbackSettings.classic);
+                    break;
+                case 2:
+                    OnSettingsDoneCallback(feedbackSettings.wordSelection);
+                    break;
+                case 3:
+                    OnSettingsDoneCallback(feedbackSettings.comprehensionLevel);
+                    break;
+                default:
+                    Debug.Log("Invalid selection!");
+                    return;
+            }
+
+            settingState = 0;
+            settingsGroup1.SetActive(true);
+            settingsGroup2.SetActive(false);
+            ShowSettings(false);
+
+        }
+        
     }
 
     public void ShowSettings(bool show)
